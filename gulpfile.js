12708f6b90c1b5dd72a049ff213da74fab520812
Jordan Bowman <desk@jrdnbwmn.com>
2017-01-24 20:34:00 -0700

    Finish my initial dev setup

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,7 @@ var gulp                    = require("gulp"),
     sass                    = require("gulp-sass"),
     combineMediaQueries     = require("gulp-combine-mq"),
     autoprefixer            = require("gulp-autoprefixer"),
-    cssmin                  = require("gulp-clean-css"),
-    rename                  = require("gulp-rename"),
-    globber                 = require('glob'),
-
-    // JS plugins
-    concat                  = require("gulp-concat"),
-    uglify                  = require("gulp-uglify"),
-
-    // Image plugin
-    imagemin                = require("gulp-imagemin"),
+    cssmin                  = require("gulp-clean-css")
 
     // General plugins
     gutil                   = require("gulp-util"),
@@ -24,64 +15,21 @@ var gulp                    = require("gulp"),
     size                    = require("gulp-size"),
     watch                   = require("gulp-watch"),
     browserSync             = require("browser-sync"),
-    cp                      = require('child_process'),
     reload                  = browserSync.reload;
 
 // Tasks
 // -------------------------------------------------------------------
-// Build site with browser-sync
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
-
-// Build the Jekyll Site
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
-
-// Rebuild Jekyll & do page reload
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-// Wait for jekyll-build, then launch the Server
-gulp.task('browser-sync', ['css', 'jekyll-build'], function() {
+// Start server
+// Note: Replace `localhost` in the browser URL with your system's
+// internal IP (something like 192.168.32.20) and then you'll be able
+// to go to that address on multiple devices and all reloading,
+// scrolling, clicking, etc will be synced across everything.
+gulp.task("browser-sync", function() {
     browserSync({
         server: {
-            baseDir: '_site'
+            baseDir: "_site"
         }
     });
-});
-
-// CSS task
-gulp.task("css", function() {
-    return gulp.src("scss/main.scss")
-        // Prevent gulp.watch from crashing
-        .pipe(plumber(onError))
-        // Compile Sass
-        .pipe(sass({ style: "compressed", noCache: true }))
-
-        // Combine media queries
-        .pipe(combineMediaQueries())
-        // parse CSS and add vendor-prefixed CSS properties
-        .pipe(autoprefixer({
-            browsers: ["last 2 versions"]
-        }))
-        // Minify CSS
-        .pipe(cssmin())
-
-        // Rename the file
-        .pipe(rename("main.css"))
-        // Show sizes of minified CSS files
-        .pipe(size({ showFiles: true }))
-        // Compile into _site/css for live injecting
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        // Compile into site for future builds
-        .pipe(gulp.dest('css'));
 });
 
 // Notify on error with a beep
@@ -92,13 +40,31 @@ var onError = function(error) {
     gutil.beep();
 };
 
-// Watch
-gulp.task('watch', function () {
-    // Watch scss files
-    gulp.watch('scss/*.scss', ['css']);
-    // Watch html/md files, run jekyll & reload browser-sync
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+// CSS task
+gulp.task("css", function() {
+    return gulp.src("_scss/main.scss")
+        // Prevent gulp.watch from crashing
+        .pipe(plumber(onError))
+        // Compile Sass
+        .pipe(sass({ style: "compressed", noCache: true }))
+        // Combine media queries
+        .pipe(combineMediaQueries())
+        // parse CSS and add vendor-prefixed CSS properties
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"]
+        }))
+        // Minify CSS
+        .pipe(cssmin())
+        // Show sizes of minified CSS files
+        .pipe(size({ showFiles: true }))
+        // Where to store the finalized CSS
+        .pipe(gulp.dest("css"));
 });
 
-// Default task will compile sass and jekyll site, launch browser-sync, & watch
-gulp.task('default', ['browser-sync', 'watch']);
+// Use default task to launch BrowserSync and watch all files
+gulp.task("default", ["browser-sync"], function () {
+    // Watch Sass files
+    watch("_scss/*", function () {
+        gulp.start('css', reload);
+    });
+});
